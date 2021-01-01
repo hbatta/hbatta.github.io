603e29b707cdf5f6c19ffd91c25a45939c707026
hari <harinarayanabatta1997@gmail.com>
2021-01-01 11:40:16 -0500

    update links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import React, { Suspense, Component } from 'react'
+// import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,46 +14,45 @@ import Projects from './components/Projects'
 export default class App extends Component {
   render() {
     return (
-      // <React.Fragment>
-      //   <NavBar />
-      //   <Router>
-      //     <Suspense fallback={<div>Loading...</div>} >
-      //       <Switch>
-      //         <Route exact path="/">
-      //           <Home />
-      //         </Route>
-      //         <Route exact path="/#experience" >
-      //           <Experience />
-      //         </Route>
-      //         <Route exact path="/#projects" >
-      //           <Projects />
-      //         </Route>
-      //         <Route exact path="/#random" >
-      //           <Random />
-      //         </Route>
-      //       </Switch>
-      //     </Suspense>
-      //   </Router>
-      // </React.Fragment>
-
       <React.Fragment>
-
-        <Home />
-        <br />
-        <br />
-        <h1 className="container">Experience</h1>
-        <Experience />
-        <br />
-        <br />
-        <h1 className="container">Projects</h1>
-        <Projects />
-        <br />
-        <br />
-        <h1 className="container">Random</h1>
-        <Random />
-        <br />
-        <br />
+        <NavBar />
+        <Router basename={process.env.PUBLIC_URL}>
+          <Suspense fallback={<div>Loading...</div>} >
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/experience" >
+                <Experience />
+              </Route>
+              <Route exact path="/projects" >
+                <Projects />
+              </Route>
+              <Route exact path="/random" >
+                <Random />
+              </Route>
+            </Switch>
+          </Suspense>
+        </Router>
       </React.Fragment>
+
+      // <React.Fragment>
+      //   <Home />
+      //   <br />
+      //   <br />
+      //   <h1 className="container">Experience</h1>
+      //   <Experience />
+      //   <br />
+      //   <br />
+      //   <h1 className="container">Projects</h1>
+      //   <Projects />
+      //   <br />
+      //   <br />
+      //   <h1 className="container">Random</h1>
+      //   <Random />
+      //   <br />
+      //   <br />
+      // </React.Fragment>
     )
   }
 }
